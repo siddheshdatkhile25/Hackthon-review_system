f41fa652fb2d7e8582d5f6bc9826de4cc3fd31f6
siddheshdatkhile25 <114308312+siddheshdatkhile25@users.noreply.github.com>
2025-11-14 08:57:11 +0530

    backend server and frontend server created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './pages/Login/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Login/>
     </>
   )
 }
