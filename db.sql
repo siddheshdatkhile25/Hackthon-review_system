0ec0639a78eaa324997d3b85b88416eefa7baf70
Tushar <tusharpawar9678@gmail.com>
2025-11-14 13:24:15 +0530

    movies dummy data

--- db.sql
+++ db.sql
@@ -39,3 +39,17 @@ CREATE TABLE shares (
     FOREIGN KEY (review_id) REFERENCES reviews(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+-- Add 10 movies (fixed) into database.
+
+INSERT INTO movies (title, release_date)
+VALUES
+('The Shawshank Redemption', '1994-10-14'),
+('The Godfather', '1972-03-24'),('The Dark Knight', '2008-07-18'),
+('Pulp Fiction', '1994-10-14'),
+('Inception', '2010-07-16'),
+('The Matrix', '1999-03-31'),
+('Interstellar', '2014-11-07'),
+('Parasite', '2019-10-11'),
+('Spirited Away', '2001-07-20'),
+('The Departed', '2006-10-06');
