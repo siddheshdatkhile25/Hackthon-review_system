a164deaa35b1fae553e0e89a7a3f40bc270c913f
Tushar <tusharpawar9678@gmail.com>
2025-11-14 09:42:52 +0530

    users routes created

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 const express = require("express")
+const userRoutes = require("./routes/userRoutes")
 
 const app = express()
+
+// Middleware to parse JSON 
+app.use(express.json());
+
+// Routes
+app.use("/users", userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Movie Reviews API Server Running')
+})
 
 app.listen(4000, 'localhost', () =>{
     console.log("Server started at port 4000")
